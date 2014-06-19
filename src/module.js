8719a71112e380a83f10b273343354850e622892
Christoph Guttandin <chrisguttandin@media-codings.com>
2014-06-19 12:18:34 +0200

    improve browserify usage
    use a browserify transform to shim the global angular object

--- src/module.js
+++ src/module.js
@@ -1,10 +1,7 @@
 'use strict';
 
-if (typeof angular === 'undefined') {
-    require('angular/angular');
-}
-
-var AudioContextServiceProvider = require('./provider/audio-context.js');
+var angular = require('angular'),
+    AudioContextServiceProvider = require('./provider/audio-context.js');
 
 module.exports = angular
     .module('chrisguttandin.audioContext', [])
