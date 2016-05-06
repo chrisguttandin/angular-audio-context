736fccbf9f7d93e11a488df2b2032f4faf860e68
Christoph Guttandin <chrisguttandin@media-codings.com>
2016-05-06 15:59:35 +0200

    remove useless variable

--- test/unit/service/audio-context.js
+++ test/unit/service/audio-context.js
@@ -4,18 +4,15 @@ var standardizedAudioContext = require('standardized-audio-context');
 
 describe('audioContextService', function () {
 
-    var audioContextService,
-        rootScope;
+    var audioContextService;
 
     beforeEach(angular.mock.module('chrisguttandin.audioContext'));
 
-    beforeEach(inject(function ($injector, $rootScope) {
-        rootScope = $rootScope;
-
+    beforeEach(inject(function ($injector) {
         audioContextService = $injector.get('audioContextService');
     }));
 
-    it('should be a instance of AudioContext', function () {
+    it('should be an instance of a AudioContext', function () {
         expect(audioContextService).to.be.an.instanceOf(standardizedAudioContext.AudioContext);
     });
 
