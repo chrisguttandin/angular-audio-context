2ffc3f8f05592261ae02ade91a0609d84b90da23
chrisguttandin <chrisguttandin@media-codings.com>
2017-03-11 03:42:57 +0100

    disable script export

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,6 +1,7 @@
 module.exports = {
     build: {
-        cmd: 'ngc -p src/tsconfig.json && rollup -c config/rollup/bundle.js && rollup -c config/rollup/script.js'
+        cmd: 'tsc -p src/tsconfig.json && rollup -c config/rollup/bundle.js'
+        // @todo Generating the script with 'rollup -c config/rollup/script.js' doesn't work right now.
     },
     continuous: {
         cmd: 'ng test'
