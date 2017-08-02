003dfa90332a5e3cbe4eb4affe548022950522ad
chrisguttandin <chrisguttandin@media-codings.com>
2017-08-02 13:30:46 +0200

    use InjectionToken instead of OpaqueToken

--- src/audio-context.module.ts
+++ src/audio-context.module.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { NgModule, OpaqueToken } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import {
     AudioContext as StandardizedAudioContextAudioContext,
     IAudioContext,
+    IAudioContextConstructor,
     isSupported as standardizedAudioContextModuleIsSupported
 } from 'standardized-audio-context';
 
-export const AudioContext = new OpaqueToken('AUDIO_CONTEXT_CONSTRUCTOR'); // tslint:disable-line:variable-name
+export { IAudioContextConstructor };
+
+export const AudioContext = new InjectionToken<typeof StandardizedAudioContextAudioContext>('AUDIO_CONTEXT_CONSTRUCTOR'); // tslint:disable-line:max-line-length variable-name
 
 export function audioContextConstructorFactory (): IAudioContext {
     return new StandardizedAudioContextAudioContext();
 }
 
-export const isSupported = new OpaqueToken('IS_SUPPORTED_PROMISE');
+export const isSupported = new InjectionToken<typeof standardizedAudioContextModuleIsSupported>('IS_SUPPORTED_PROMISE');
 
 export function isSupportedFactory () {
     return standardizedAudioContextModuleIsSupported;
