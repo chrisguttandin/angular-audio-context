016dc4c8826766317fdfede1e9d95c08b76c7713
chrisguttandin <chrisguttandin@media-codings.com>
2017-09-05 01:10:00 +0200

    feat: implement forRoot() and forChild() methods

--- src/audio-context.module.ts
+++ src/audio-context.module.ts
@@ -1,8 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { InjectionToken, NgModule } from '@angular/core';
-import { AudioContext, IAudioContext, isSupported as standardizedAudioContextModuleIsSupported } from 'standardized-audio-context';
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import {
+    AudioContext,
+    IAudioContext,
+    IAudioContextOptions,
+    isSupported as standardizedAudioContextModuleIsSupported
+} from 'standardized-audio-context';
 
 export { AudioContext, IAudioContext };
+
+export function createAudioContextFactory (latencyHint: IAudioContextOptions['latencyHint']) {
+    return function () {
+        return new AudioContext({ latencyHint });
+    };
+}
 
 export const isSupported = new InjectionToken<typeof standardizedAudioContextModuleIsSupported>('IS_SUPPORTED_PROMISE');
 
@@ -15,8 +26,24 @@ export function isSupportedFactory () {
         CommonModule
     ],
     providers: [
-        AudioContext,
         { provide: isSupported, useFactory: isSupportedFactory }
     ]
 })
-export class AudioContextModule { }
+export class AudioContextModule {
+
+    public static forRoot (latencyHint?: IAudioContextOptions['latencyHint']): ModuleWithProviders {
+        return {
+            ngModule: AudioContextModule,
+            providers: [
+                { provide: AudioContext, useFactory: createAudioContextFactory(latencyHint) }
+            ]
+        };
+    }
+
+    public static forChild (): ModuleWithProviders {
+        return {
+            ngModule: AudioContextModule
+        };
+    }
+
+}
