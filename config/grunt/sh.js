5da2f9cc4936866cd5a06e9343a3362a60f0f075
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-10-09 17:50:59 +0200

    feat: export es2018 code

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,12 +1,9 @@
 module.exports = {
-    'build-es2015': {
+    'build-es2018': {
         cmd: 'ngc -p src/tsconfig.json'
     },
     'build-es5': {
         cmd: 'rollup -c config/rollup/bundle.js'
-    },
-    'build-esm': {
-        cmd: 'ngc -p src/tsconfig.esm.json'
     },
     'continuous': {
         cmd: 'ng test'
